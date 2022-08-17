e69629dd0fd5018734d56a4e0cea64f7ce374f05
mnzh63 <danilocsi88@gmail.com>
2022-08-16 22:17:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const options = {
-    origin: "https://my-chat-online.herokuapp.com"
-}
 
-app.use(cors(options))
+
+app.use(cors())
 app.use('/', express.static(path.join(__dirname, 'src/public')))
 
 
