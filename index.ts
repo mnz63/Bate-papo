4d72f5d171455c594d4b133778f819f2f3e52f44
mnzh63 <danilocsi88@gmail.com>
2022-08-16 21:52:56 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
-app.use('/chat', express.static(path.join(__dirname, 'src/public')))
+app.use('/', express.static(path.join(__dirname, 'src/public')))
 
 
 const server = app.listen(PORT, ()=>{
